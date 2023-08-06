5bbe7d0c6377a1639946ad975ca922e14530ce11
Matz3e <42473379+Matz3e@users.noreply.github.com>
2023-08-06 20:48:39 +0200

    Create csv_parser.py

---
+++ AP-Staging/csv_parser.py
@@ -0,0 +1,14 @@
+import csv
+
+def parse_csv(file_path):
+    data = []
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+    return data
+
+if __name__ == "__main__":
+    csv_path = sys.argv[1]
+    parsed_data = parse_csv(csv_path)
+    print(parsed_data)
